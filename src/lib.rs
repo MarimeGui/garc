c20b33f3bff14f74305973857ce9bb8fd62d281a
MarimeGui <lepro.guillaume@gmail.com>
2019-04-28 16:16:43 +0200

    Error created, GARC import

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,17 @@
 extern crate ez_io;
 
-pub struct GARC {
+pub mod error;
 
+use crate::error::GARCError;
+use std::io::Read;
+
+type Result<T> = ::std::result::Result<T, GARCError>;
+
+pub struct GARC {}
+
+impl GARC {
+    pub fn import<R: Read>(reader: &mut R) -> Result<GARC> {
+        let garc = GARC {};
+        Ok(garc)
+    }
 }
